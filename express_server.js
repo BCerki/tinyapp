8949cea0430299c98eb9828957a8fb7620540c68
Brianna <briannac@shaw.ca>
2021-04-12 21:16:25 +0000

    added /hello response

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -14,6 +15,10 @@ app.get('/', (request, response) => {
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
 })
+
+app.get('/hello',(request,response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
