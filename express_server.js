9137cd4363ac1c8d3fb447a48baf2a5330389f69
Brianna <briannac@shaw.ca>
2021-04-14 01:52:40 +0000

    added registration form and GET /register endpoint

--- express_server.js
+++ express_server.js
@@ -42,9 +42,14 @@ app.post('/logout', (req,res) => {
   res.redirect('/urls');
 });
 
+app.get('/register', (req, res)=> {
+  const templateVars = { username: req.cookies["username"]};
+  res.render('registration',templateVars)
+});
+
 app.get('/urls/new', (req, res) => {
   const templateVars = { username: req.cookies["username"]}
-  console.log(req);
+  // console.log(req);
   res.render('urls_new',templateVars);
 });
 
