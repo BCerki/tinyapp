3fed46f541ad09044e61cc7e8b4fa448e8a7fd49
Brianna <briannac@shaw.ca>
2021-04-16 03:29:16 +0000

    cleanup and linting of test/helpersTest.js

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -5,13 +5,13 @@ const {generateRandomString,getUserByEmail, urlsForUser, isLoggedIn, urlIsOwnedB
 //test object provided by Compass for checking getUserByEmail
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -33,7 +33,7 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
 
 const req = {
   session: {
@@ -46,13 +46,13 @@ const req = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     assert.equal(user, expectedOutput);
   });
 
   it('should return undefined if the user isn\'t in the database', function() {
-    const user = getUserByEmail("user1@example.com", testUsers)
+    const user = getUserByEmail("user1@example.com", testUsers);
     const expectedOutput = undefined;
     assert.strictEqual(user, expectedOutput);
   });
@@ -75,17 +75,3 @@ describe('urlIsOwnedByUser', function() {
 
   
 });
-
-
-
-
-//template
-// describe('', function() {
-//   it('', function() {
-//     const thing = 0;
-//     const expectedOutput = "userRandomID";
-//     assert.equal(thing, expectedOutput);
-//   });
-
-  
-// });
